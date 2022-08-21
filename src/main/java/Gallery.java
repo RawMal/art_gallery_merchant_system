4f347447fe1dc7fe3f13be054033470093295c8e
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-21 17:18:02 +0100

    Art Gallery Class

---
+++ src/main/java/Gallery.java
@@ -0,0 +1,41 @@
+public class Gallery {
+
+    double till;
+    String galleryName;
+    String artCollection;
+
+
+    public Gallery(double till, String galleryName, String artCollection){
+        this.till = till;
+        this.galleryName = galleryName;
+        this.artCollection = artCollection;
+    }
+
+    public double gettill(){
+        return till;
+    }
+
+    public void settill(double till){
+        this.till = till;
+    }
+
+    public String getgalleryName(String galleryName){
+        return galleryName;
+    }
+
+    public void setGalleryName(String galleryName){
+        this.galleryName = galleryName;
+    }
+
+    public String getArtCollection(){
+        return artCollection;
+    }
+
+    public void setArtCollection(String artCollection){
+        this.artCollection = artCollection;
+    }
+
+
+
+
+}
