3a1d46d0992dccc81c14f7a01b7329080dd42baa
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-22 13:57:01 +0100

    Add Artist testing:

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CustomerTest {
+    Customer customer;
+    Artist leonardoDavinci;
+    Artwork monaLisa;
+    Artist vincentVanGogh;
+    Artwork irises;
+
+    @BeforeEach
+    public void setUp(){
+        customer = new Customer("Scrooge McDuck", 23_000);
+
+        leonardoDavinci = new Artist("Leonardo DaVinci");
+        monaLisa = new Artwork("Mona Lisa", leonardoDavinci, 1_000, 846);
+
+        vincentVanGogh = new Artist("Vincent Van Gogh");
+        irises = new Artwork("Irises", vincentVanGogh, 50_000_000, 885);
+    }
+
+    @Test
+    public void canAffordArtwork__true(){
+        assertThat(customer.canBuy(monaLisa)).isEqualTo(true);
+    }
+
+    @Test
+    public void canAffordArtwork__false(){
+        assertThat(customer.canBuy(irises)).isEqualTo(false);
+    }
+
+    @Test
+    public void customerArtworkCountStartsEmpty(){
+        assertThat(customer.getArtworkCount()).isEqualTo(0);
+    }
+
+    @Test
+    public void canBuyArtwork(){
+        customer.buyArtwork(monaLisa);
+        assertThat(customer.getWallet()).isEqualTo(22_000);
+        assertThat(customer.getArtworkCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void hasName(){
+        assertThat(customer.getCustomerName()).isEqualTo("Scrooge McDuck");
+    }
+
+    @Test
+    public void hasWallet(){
+        assertThat(customer.getWallet()).isEqualTo(23_000);
+    }
+}
